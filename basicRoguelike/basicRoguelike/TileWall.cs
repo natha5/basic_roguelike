122c03d4709043bc4bb3d54c4feee85d44450be6
natha5 <ns539@canterbury.ac.uk>
2022-08-24 17:12:07 +0100

    <mod> TileWall class variables set

--- basicRoguelike/basicRoguelike/TileWall.cs
+++ basicRoguelike/basicRoguelike/TileWall.cs
@@ -5,7 +5,16 @@ using Microsoft.Xna.Framework;
 namespace basicRoguelike
 {
     //TileWall based on TileBase
-    class TileWall
+    public class TileWall : TileBase
     {
+        //Default constructor
+        //Walls are set to block movement and line of sight
+        //grey foreground and transparent background
+        //represetned by # symbol
+
+        public TileWall(bool blocksMovement=true, bool blocksLOS=true) : base(Color.LightGray, Color.Transparent, '#', blocksMovement, blocksLOS)
+        {
+            Name = "Wall";
+        }
     }
 }
