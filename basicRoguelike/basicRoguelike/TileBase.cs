9d38a419f65d49eb016e2e67de269e8ae2729a9c
natha5 <ns539@canterbury.ac.uk>
2022-08-24 17:00:55 +0100

    <add> TileBase class

---
+++ basicRoguelike/basicRoguelike/TileBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using SadConsole;
+
+namespace basicRoguelike
+{
+    //abstract base class representing the most basic form of all tiles used
+
+    public abstract class TileBase : Cell
+    {
+        //Default constructor
+        public TileBase() : base()
+        {
+
+        }
+    }
+}
